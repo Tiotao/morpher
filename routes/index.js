3fafdcba71747720dad9150a8701f16f9d08abdf
Zhang Yiwen <tiotaocn@gmail.com>
2015-11-09 23:26:40 +0800

    add heroku

--- routes/index.js
+++ routes/index.js
@@ -106,7 +106,6 @@ function decodeAndSaveImage(base64Data, uploadLocation, fileName){
 }
 
 router.post('/createMorph', function(req, res, next) {
-	console.log(req.body)
 	var folderName = req.body.id;
 	var startBase64 = req.body.start;
 	var endBase64 = req.body.end;
@@ -117,40 +116,6 @@ router.post('/createMorph', function(req, res, next) {
 	decodeAndSaveImage(endBase64, saveDir, "end");
 	mesh(saveDir + '/', folderName, function(data){
 		res.json(fs.readdirSync(saveDir + '/gif'));
-		// gm().in(saveDir + '/*.jpg').out('animate.gif').stream(function (err, stdout, stderr) {
-		// 	console.log(err)
-		// 	res.send('success')
-		// });
-
-		// var GIFEncoder = require('gifencoder');
-		// var Canvas = require('canvas');
-		// var encoder = new GIFEncoder(444, 444);
-		// encoder.createReadStream().pipe(fs.createWriteStream('myanimated.gif'));
-		// encoder.start();
-		// encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
-		// encoder.setDelay(500);  // frame delay in ms
-		// encoder.setQuality(10); // image quality. 10 is default.
-
-		// var canvas = new Canvas(320, 240);
-		// var ctx = canvas.getContext('2d');
-		// // red rectangle
-		// ctx.fillStyle = '#ff0000';
-		// ctx.fillRect(0, 0, 320, 240);
-		// encoder.addFrame(ctx);
-
-		// // green rectangle
-		// ctx.fillStyle = '#00ff00';
-		// ctx.fillRect(0, 0, 320, 240);
-		// encoder.addFrame(ctx);
-
-		// // blue rectangle
-		// ctx.fillStyle = '#0000ff';
-		// ctx.fillRect(0, 0, 320, 240);
-		// encoder.addFrame(ctx);
-
-		// encoder.finish();
-
-		
 	})
 	
 });
